767a8bf4cd416fae0260c79381e19612938d9c43
Dalish <Dalish@DESKTOP-S7S9PJT>
2020-08-04 14:55:24 +0200

    Added mockup WorkerView and WorkerViewModel.

---
+++ HotelApp/ViewModel/WorkerViewModel.cs
@@ -0,0 +1,45 @@
+﻿using HotelApp.Command;
+using HotelApp.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HotelApp.ViewModel
+{
+    class WorkerViewModel
+    {
+        WorkerView view;
+
+        public WorkerViewModel(WorkerView wv)
+        {
+            view = wv;
+        }
+
+
+        private ICommand logout;
+        public ICommand Logout
+        {
+            get
+            {
+                if (logout == null)
+                {
+                    logout = new RelayCommand(param => LogoutExecute(), param => CanLogoutExecute());
+                }
+                return logout;
+            }
+        }
+        private void LogoutExecute()
+        {
+            LoginView logout = new LoginView();
+            view.Close();
+            logout.Show();
+        }
+        private bool CanLogoutExecute()
+        {
+            return true;
+        }
+    }
+}
